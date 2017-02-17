77ea15ae8a5f56768f28594b355b7f8accb33f27
Ankur Jain <ankurjain8448@gmail.com>
2017-02-17 17:09:41 +0530

    Singly Linklist

--- DS/dfs.py
+++ DS/dfs.py
@@ -10,21 +10,38 @@ class Graph(object):
 		self.graph[u].append(v)
 
 	def dfs(self, start_node):
+		"""
+			Print all nodes reachable from start_node, not all the nodes, i.e it 
+			will not touch those nodes which are not connected to start_node via any path
+		"""
 		self.visited = [False]*self.nodes
+		print "dfs_recursive"
 		self.dfs_recursive(start_node)
+		print "==="
+		print "dfs_iterative"
+		self.visited = [False]*self.nodes
+		self.dfs_iterative(start_node)
 
 	def dfs_recursive(self, start_node):
 		"""
 			Don't call me directly # call dfs
 		"""
-		# print ">> dfs for : ", start_node
 		if not self.visited[start_node]:
 			self.visited[start_node] = True
 			print "visited : ", start_node
 			for each_node in self.graph[start_node]:
 				self.dfs_recursive(each_node)
-		# else:
-		# 	print "already visited : ", start_node
+
+	def dfs_iterative(self, start_node):
+		stack = [start_node]
+		while stack:
+			temp = stack.pop()
+			if not self.visited[temp]:
+				print "visited : ", temp
+				self.visited[temp] = True
+				for each_node in self.graph[temp]:
+					if not self.visited[each_node]:
+						stack.append(each_node)
 
 	def print_graph(self):
 		print "========== GRAPH ============="
@@ -40,7 +57,8 @@ if __name__ == '__main__':
 	[2,4],
 	[1,],
 	[1,2,],
-	[4,1,3],
+	[1,3,5],
+	[]
 	]
 	nodes = len(Input)
 	g = Graph(nodes)
--- DS/linklist.py
+++ DS/linklist.py
@@ -1,3 +1,36 @@
-class Linklist(object):
-	def __init__(self, arg):
-		pass		
+class Node(object):
+	"""docstring for Node"""
+	def __init__(self, val):
+		self.val = val
+		self.next = None
+		
+
+class SingleLinklist(object):
+	def __init__(self):
+		self.head = None
+		self.start = None
+		self.current = None
+
+	def add_node(self, node):
+		node = Node(node)
+		if self.head:
+			self.current.next = node
+			self.current = self.current.next
+		else:
+			self.head = node
+			self.current = self.head
+		return self.head
+
+	def print_linklist(self):
+		temp = self.head
+		while temp:
+			print temp.val
+			temp = temp.next
+
+
+if __name__ == '__main__':
+	ll = SingleLinklist()
+	nodes = 10
+	for i in xrange(10):
+		ll.add_node(i)
+	ll.print_linklist()
