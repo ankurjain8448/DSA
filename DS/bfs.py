b26a4c339566584ec5ac80e6018d5aed7998cbc2
Ankur Jain <ankurjain8448@gmail.com>
2017-02-17 18:32:21 +0530

    bfs

---
+++ DS/bfs.py
@@ -0,0 +1,25 @@
+from graph import Graph
+
+class Graph(Graph):
+	
+	def bfs(self, start_node):
+		queue = [start_node]
+		while queue:				
+			temp = queue.pop(0)
+			if not self.visited[temp]:
+				print temp
+				self.visited[temp] = True
+				for each in self.graph[temp]:
+					if not self.visited[each]:
+						queue.append(each)
+
+
+
+if __name__ == '__main__':
+	Input = [[2,4], [1,],[1,2,8,9],[1,3,5],[6,7],[],[],[],[]]
+	nodes = len(Input)
+
+	g = Graph(nodes)
+
+	g.fill_graph(Input)
+	g.bfs(1)
